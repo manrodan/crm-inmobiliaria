4d8fff5574bef5c41bced9dd829c9196498e616b
Manuel Rodriguez <manrodan@hotmail.com>
2026-02-11 12:26:03 +0100

    Add explicit redirect from / to /dashboard

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // Force redeploy
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
